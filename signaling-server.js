c0626ea977bfd70b4ee033788c3597c515c55416
Melkor [Zhiltsov Arseney] <nimondars@gmail.com>
2018-12-11 01:02:15 +0300

    initial upload

---
+++ signaling-server.js
@@ -0,0 +1,106 @@
+var PORT = 8080;
+
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser')
+var main = express()
+var server = http.createServer(main)
+var io  = require('socket.io').listen(server);
+
+server.listen(PORT, null, function() {
+    console.log("Listening on port " + PORT);
+});
+
+main.get('/', function(req, res){ res.sendFile(__dirname + '/client.html'); });
+
+var channels = {};
+var sockets = {};
+
+ 
+
+io.sockets.on('connection', function (socket) {
+    socket.channels = {};
+    sockets[socket.id] = socket;
+
+    console.log("["+ socket.id + "] connection accepted");
+    socket.on('quit', function () {
+        for (var channel in socket.channels) {
+            part(channel);
+        }
+        console.log("["+ socket.id + "] disconnected");
+        delete sockets[socket.id];
+    });
+	
+	socket.on('save', function (config){
+		const fs = require('fs');
+		
+		fs.writeFile('sdp.txt', config.session_description, (err) =>{
+			if (err) throw err;
+			
+			console.log("sdp saved");
+		})
+	});
+
+    socket.on('create_join', function (config) {
+        console.log("["+ socket.id + "] join ", config);
+        var channel = config.channel;
+        var userdata = config.userdata;
+
+        if (channel in socket.channels) {
+            console.log("["+ socket.id + "] ERROR: already joined ", channel);
+            return;
+        }
+
+        if (!(channel in channels)) {
+            channels[channel] = {};
+        }
+
+        for (id in channels[channel]) {
+            channels[channel][id].emit('joined', {'peer_id': socket.id, 'offer': false});
+            socket.emit('joined', {'peer_id': id, 'offer': true});
+        }
+
+        channels[channel][socket.id] = socket;
+        socket.channels[channel] = channel;
+    });
+
+    function part(channel) {
+        console.log("["+ socket.id + "] part ");
+
+        if (!(channel in socket.channels)) {
+            console.log("["+ socket.id + "] ERROR: not in ", channel);
+            return;
+        }
+
+        delete socket.channels[channel];
+        delete channels[channel][socket.id];
+
+        for (id in channels[channel]) {
+            channels[channel][id].emit('removePeer', {'peer_id': socket.id});
+            socket.emit('removePeer', {'peer_id': id});
+        }
+    }
+    socket.on('part', part);
+
+    socket.on('relayICECandidate', function(config) {
+        var peer_id = config.peer_id;
+        var ice_candidate = config.ice_candidate;
+        console.log("["+ socket.id + "] relaying ICE candidate to [" + peer_id + "] ", ice_candidate);
+
+        if (peer_id in sockets) {
+            sockets[peer_id].emit('iceCandidate', {'peer_id': socket.id, 'ice_candidate': ice_candidate});
+        }
+    });
+
+    socket.on('ready', function(config) {
+        var peer_id = config.peer_id;
+        var session_description = config.session_description;
+		var fs = require('fs');
+		
+        console.log("["+ socket.id + "] relaying session description to [" + peer_id + "] ", session_description);
+		
+        if (peer_id in sockets) {
+            sockets[peer_id].emit('offer', {'peer_id': socket.id, 'session_description': session_description});
+        }
+    });
+});
